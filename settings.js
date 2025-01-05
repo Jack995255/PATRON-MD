9bc2b7336e15556e17e2ef9dc306c13d0becada4
Jack995255 <ananth8220034270@gmail.com>
2025-01-05 09:07:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "TOXICMD"
 
-global.banknumber = "8133729715"
+global.banknumber = "13156336630"
 
-global.bankowner = "FADARE"
+global.bankowner = "TOXIC"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
